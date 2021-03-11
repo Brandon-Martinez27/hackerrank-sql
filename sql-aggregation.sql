abdfeddee314b1f05fe46898a27a29894320cee6
“Brandon <Brandon.joseph.martinez3@gmail.com>
2021-03-10 22:04:31 -0600

    working on sql agg problem

--- sql-aggregation.sql
+++ sql-aggregation.sql
@@ -45,3 +45,15 @@ Write a query calculating the amount of error (i.e.:  average monthly salaries),
 
 SELECT CEILING(AVG(Salary) - AVG(REPLACE(Salary,'0','')))
 FROM EMPLOYEES;
+
+-- Top Earners
+/* We define an employee's total earnings to be their monthly salary x months worked, 
+and the maximum total earnings to be the maximum total earnings for any employee in the Employee table. 
+Write a query to find the maximum total earnings for all employees as well as the total number of employees who have maximum total earnings. 
+Then print these values as  space-separated integers.
+*/
+
+SELECT SUM(salary * months) as earnings, COUNT(*)
+FROM employee
+GROUP BY employee_id
+ORDER BY earnings DESC;
