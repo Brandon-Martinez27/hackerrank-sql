1e1fc2fef4379a4b96428cc9d861cd995506bece
“Brandon <Brandon.joseph.martinez3@gmail.com>
2021-03-11 12:30:59 -0600

    completed 5 new porblems

--- sql-aggregation.sql
+++ sql-aggregation.sql
@@ -53,7 +53,30 @@ Write a query to find the maximum total earnings for all employees as well as th
 Then print these values as  space-separated integers.
 */
 
-SELECT SUM(salary * months) as earnings, COUNT(*)
-FROM employee
-GROUP BY employee_id
-ORDER BY earnings DESC;
+SELECT (salary * months) as earnings, count(*) 
+FROM employee 
+GROUP BY earnings
+ORDER BY earnings DESC 
+LIMIT 1;
+
+-- Weather Observation Station 2
+/* 
+Query the following two values from the STATION table:
+1. The sum of all values in LAT_N rounded to a scale of decimal places.
+2. The sum of all values in LONG_W rounded to a scale of  decimal places.
+*/
+
+SELECT ROUND(SUM(LAT_N), 2), ROUND(SUM(LONG_W), 2)
+FROM STATION;
+
+-- Weather Observation Station 13
+/* 
+Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7880 
+and less than 137.2345. 
+Truncate your answer to 4 decimal places.
+*/
+
+SELECT ROUND(SUM(LAT_N),4) as lat_sum
+FROM STATION 
+WHERE LAT_N > 38.7880
+    AND LAT_N < 137.2345;
