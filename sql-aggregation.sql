027af340f7aabd4dab59c3e3e06795b0ec96421b
“Brandon <Brandon.joseph.martinez3@gmail.com>
2021-04-23 14:08:43 -0500

    completed aggregation challenges

--- sql-aggregation.sql
+++ sql-aggregation.sql
@@ -139,5 +139,41 @@ Query the Manhattan Distance between points P_1 and P_2 and round it to a scale 
 */
 
 
-SELECT ROUND(ABS(MIN(LAT_N) - MAX(LAT_N)) + ABS(MIN(LONG_W) - MAX(LONG_W)), 4)
+SELECT ROUND(
+    ABS(MIN(LAT_N) - MAX(LAT_N)) + 
+    ABS(MIN(LONG_W) - MAX(LONG_W)), 4
+            )
 FROM STATION;
+
+-- Weather Observation Station 19
+/* 
+Consider P_1(a,c) and P_2(b,d) to be two points on a 2D plane.
+where (a,b) are the respective minimum and maximum values of Northern Latitude (LAT_N) and 
+(c,d) are the respective minimum and maximum values of Western Longitude (LONG_W) in STATION.
+Query the Euclidean Distance between points P_1 and P_2 and format your answer to display 4 decimal digits.
+*/
+
+SELECT ROUND(
+    SQRT(
+        POWER(MAX(LAT_N) - MIN(LAT_N), 2) + 
+        POWER(MAX(LONG_W) - MIN(LONG_W), 2)
+        ), 4
+            )
+FROM STATION;
+
+-- Weather Observation Station 20
+/* 
+A median is defined as a number separating the higher half of a data set from the lower half. 
+Query the median of the Northern Latitudes (LAT_N) from STATION and round your answer to 4 decimal places.
+*/
+
+
+SELECT ROUND(S.LAT_N,4)
+FROM STATION as S
+WHERE (
+    SELECT COUNT(*) 
+    FROM STATION 
+    WHERE LAT_N < S.LAT_N ) = (
+        SELECT COUNT(*) 
+        FROM STATION 
+        WHERE LAT_N > S.LAT_N)
